ff6e7ad5f500c4cf79f1e8e51d70456a6e01b06c
Tianhao05875 <them05875@gmail.com>
2025-02-19 15:39:36 +0800

    Update 123.html

--- 123.py
+++ 123.py
@@ -1,1 +1,12 @@
-print("hellowold:)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>体重提示</title>
+</head>
+<body>
+    <div style="text-align: center; margin-top: 100px;">
+        <h1>您的体重过轻</h1>
+    </div>
+</body>
+</html>
